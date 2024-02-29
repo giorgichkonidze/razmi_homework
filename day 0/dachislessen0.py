f2b7546f52f9c93bdfef6f2f5a24b2816a5750c8
giorgichkonidze <160318100+giorgichkonidze@users.noreply.github.com>
2024-02-29 21:18:28 +0400

    Add files via upload

---
+++ day 0/dachislessen0.py
@@ -0,0 +1,15 @@
+#user data
+
+
+name=input("user name")
+print=("user's name is" + " " "name")
+
+surname=input("user's surname")
+print=("user's surname")
+
+age=int(input("user's birth year"))
+print=("users's birth year")
+
+
+
+
